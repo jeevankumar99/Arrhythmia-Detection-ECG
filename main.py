31c25c9a031b973f76d413f4aae454586b2eb835
Jeevan Kumar <rg1kumar1@gmail.com>
2022-07-02 22:04:34 +0530

    95% acc - Batch size and epochs adjusted!

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import class_weight, resample
 from utils import *
 
+from keras.utils.np_utils import to_categorical
+
+
 LABELS = {
     0: 'N', 
     1: 'S',
@@ -14,8 +17,40 @@ LABELS = {
     4: 'Q'
 }
 
+EPOCHS = 5
+
 if __name__ == "__main__":
     data_path = "mitbih_ecg_dataset"
     train_data, test_data = load_dataset(data_path)
     train_data = balance_train_data(train_data)
+
+    Y_train = to_categorical(train_data[187])
+    y_test = to_categorical(test_data[187])
+
+    X_train = train_data.iloc[:, :187].values
+    x_test = test_data.iloc[:, :187].values
+
+    print ("Shape: ", x_test.shape)
+    X_train = X_train.reshape(len(X_train), X_train.shape[1], 1)
+    x_test = x_test.reshape(len(x_test), x_test.shape[1], 1)
+
+    callbacks = [EarlyStopping(monitor='val_loss', patience=8), ModelCheckpoint(filepath='./best_weights.h5', monitor='val_loss', save_best_only=True)]
+
+    model = load_model(X_train.shape)
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    model.summary()
+
+    model.fit(
+        X_train, Y_train, 
+        epochs=EPOCHS, validation_data=(x_test,y_test))
+
+    score = model.evaluate(x_test,y_test, verbose=1)
+    y_predict = model.predict(x_test)
+
+    model.save("ecg_cnn2.h5")
+    print (y_predict)
+
+
+
+
     
--- utils.py
+++ utils.py
@@ -2,8 +2,11 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.utils import class_weight, resample
+
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, BatchNormalization
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
 def load_dataset(data_path):
@@ -17,26 +20,77 @@ def load_dataset(data_path):
     return train_data, test_data
 
 def balance_train_data(train_data):
-    labels = train_data[187].value_count()
-    print("Class Distribution:-", labels)
+    labels = train_data[187].value_counts()
+    print("Class Distribution:-\n", labels)
 
-    train_0 = train_data[train_data[187] == 0.0].sample(n=1600, random_state=1)
+    train_0 = train_data[train_data[187] == 0.0].sample(n=16000, random_state=1)
     train_1 = train_data[train_data[187] == 1.0]
     train_2 = train_data[train_data[187] == 2.0]
     train_3 = train_data[train_data[187] == 3.0]
     train_4 = train_data[train_data[187] == 4.0]
 
-    train_1_bal = resample(train_1, replace=True, n_samples=1600, random_state=1)
-    train_2_bal = resample(train_2, replace=True, n_samples=1600, random_state=1)
-    train_3_bal = resample(train_3, replace=True, n_samples=1600, random_state=1)
-    train_4_bal = resample(train_4, replace=True, n_samples=1600, random_state=1)
+    train_1_bal = resample(train_1, replace=True, n_samples=16000, random_state=1)
+    train_2_bal = resample(train_2, replace=True, n_samples=16000, random_state=1)
+    train_3_bal = resample(train_3, replace=True, n_samples=16000, random_state=1)
+    train_4_bal = resample(train_4, replace=True, n_samples=16000, random_state=1)
 
     balanced_train = pd.concat([train_0, train_1_bal, train_2_bal, train_3_bal, train_4_bal])
-    balanced_labels = balance_train_data[187].value_count()
+    balanced_labels = balanced_train[187].value_counts()
+
+    n_sample = train_0.sample(600)
+    s_sample = train_1.sample(600)
+    v_sample = train_2.sample(600)
+    f_sample = train_3.sample(600)
+    q_sample = train_4.sample(600)
+
+    fig, axis = plt.subplots(5, sharex=True, sharey=True)
+    fig.set_size_inches(10, 10)
+
+    axis[0].set_title("None echoic beats")
+    axis[0].plot(n_sample.iloc[0, :186])
+
+    axis[1].set_title("Superventricular Echoic beats")
+    axis[1].plot(s_sample.iloc[0, :186])
+
+    axis[2].set_title("Ventricular Ectopic beats")
+    axis[2].plot(v_sample.iloc[0, :186])
+
+    axis[3].set_title("Fusion beats")
+    axis[3].plot(f_sample.iloc[0, :186])
+
+    axis[4].set_title("Unknown beats")
+    axis[4].plot(q_sample.iloc[0, :186])
+
+    plt.show()
+
     print ("Class Distribution after balancing:-\n", balanced_labels)
 
     return balanced_train
 
 
+def load_model(train_shape):
+    model = Sequential()
+    model.add(Conv1D(128,3,input_shape=(train_shape[1],1), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=2))
+    
+    model.add(Conv1D(64,3, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=2))
+    
+    model.add(Conv1D(64,2, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=2))
+    
+    model.add(Conv1D(64,2, activation='relu'))
+    model.add(Flatten())
+    
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(5, activation='softmax'))
+
+    return model
 
 
